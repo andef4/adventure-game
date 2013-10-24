4e6e3334061453f53a5035cb95628384269d4852
Fabio Anderegg <andef4@bfh.ch>
2013-10-24 18:41:23 +0200

    added initial skill loader

---
+++ src/src/main/java/ch/andefgassm/adventuregame/game/assets/AssetLoadException.java
@@ -0,0 +1,20 @@
+package ch.andefgassm.adventuregame.game.assets;
+
+public class AssetLoadException extends Exception {
+    private static final long serialVersionUID = 1L;
+    
+    
+    public AssetLoadException(String message) {
+        super(message);
+    }
+    
+    public AssetLoadException(Throwable cause) {
+        super(cause);
+    }
+    
+    public AssetLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+}
