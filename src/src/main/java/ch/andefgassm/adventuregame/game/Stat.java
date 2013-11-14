62d82f07d9687ae70389cbeaa44c97ed9e6e88fe
gassm9 <matthiasgasser@147-87-143-33.bfh.ch>
2013-11-14 21:30:08 +0100

    implemented function to equip items

--- src/src/main/java/ch/andefgassm/adventuregame/game/Stat.java
+++ src/src/main/java/ch/andefgassm/adventuregame/game/Stat.java
@@ -3,5 +3,8 @@ package ch.andefgassm.adventuregame.game;
 import ch.andefgassm.adventuregame.combat.IStat;
 
 public enum Stat implements IStat {
-    STRENGTH;
+    STRENGTH,
+    AGILITY,
+    SPEED,
+    BASMATI,
 }
--- src/src/main/java/ch/andefgassm/adventuregame/game/state/InventoryMenuState.java
+++ src/src/main/java/ch/andefgassm/adventuregame/game/state/InventoryMenuState.java
@@ -1,18 +1,43 @@
 package ch.andefgassm.adventuregame.game.state;
 
 import java.util.List;
-
+import java.util.Map.Entry;
+import ch.andefgassm.adventuregame.combat.IStat;
+import ch.andefgassm.adventuregame.game.Stat;
 import ch.andefgassm.adventuregame.game.inventory.Item;
+import ch.andefgassm.adventuregame.game.inventory.ItemType;
 import ch.andefgassm.adventuregame.game.inventory.Player;
 
 public class InventoryMenuState extends AbstractConsoleGameState {
 	
+	private GameStateContext context;
+	private Player p = Player.getInstance();
+	List<Item> inventory = p.getInventory();
 	//character mit items ausruesten	
 	
-	public void init(GameStateContext context) {
+	public InventoryMenuState() {
+		//testItem
+		Item testItem = new Item();
+		testItem.setName("Todes-Messer-NunJackOh");
+		testItem.setType(ItemType.WEAPON);
+		testItem.getStats().put(Stat.STRENGTH, 10);
+		testItem.getStats().put(Stat.BASMATI, 99);
+		inventory.add(testItem);		
+		//testItem
 		
-		new Player();
-		Player p = Player.getInstance();
+		//testItem
+		Item testItem2 = new Item();
+		testItem2.setName("Affenschaedel-Helm");
+		testItem2.setType(ItemType.HEAD);
+		testItem2.getStats().put(Stat.STRENGTH, 69);
+		testItem2.getStats().put(Stat.SPEED, 3);
+		testItem2.getStats().put(Stat.AGILITY, 19);
+		inventory.add(testItem2);		
+		//testItem
+	}
+	
+	public void init(GameStateContext context) {
+		this.context = context;
 		
 	    clear();
 		println("Inventory Menu");
@@ -20,34 +45,48 @@ public class InventoryMenuState extends AbstractConsoleGameState {
 		println("");
 		println("Inventory (press button to equip item)");
 		println("--------------------------------------");
-		
-		List<Item> inventory = p.getInventory();
-		
-		//testItem
-		Item testItem = new Item();
-		testItem.setName("Todes-Messer-NunJackOh");
-		inventory.add(testItem);		
-		//testItem
-				
+					
 		for(int i=1; i <= inventory.size(); i++)
 		{
+			Item item = inventory.get(i-1);
+			StringBuilder sb = new StringBuilder(i);
+			sb.append(String.format("%s ) ", i));
+			sb.append(String.format("[%s] ", isItemEquipped(item)));
+			sb.append(item.getType());
+			sb.append(" ");
+			sb.append(item.getName());
+			sb.append(" ");
 			
-			println(i + ") " + inventory.get(i-1).getName().toString());
+			for (Entry<IStat, Integer> entry : item.getStats().entrySet()) {
+				sb.append(String.format("[%s %s] ", entry.getKey(), entry.getValue()));
+			}
+			
+			println(sb.toString());
 		}
 		
+	}
 
-//		println("1) [x] Axe [20 Stamina, 30 Strength]");
-//		println("2) [ ] Shield [25 Stamina, 20 Strength]");
-//		println("");
-		
-		//getInt("Your choice:", 1, 1);
-		
-		context.changeState(GameStateContext.MAIN_MENU);
+	private String isItemEquipped(Item item) {
+		boolean hasMatch = false;
+		for (Entry<ItemType, Item> entry : p.getEquipment().entrySet()) {
+			if(entry.getValue() == item)
+				hasMatch = true;
+		}
+
+		if(hasMatch)
+			return "X";
+		else
+			return "";
 	}
 
 	@Override
 	public void handleInput(int input) {
-		
+		if(input == 0){
+			context.changeState(GameStateContext.MAIN_MENU);
+		}
+		else if(input <= inventory.size()){
+			p.equip(inventory.get(input-1));
+			init(context);
+		}
 	}
-
 }
