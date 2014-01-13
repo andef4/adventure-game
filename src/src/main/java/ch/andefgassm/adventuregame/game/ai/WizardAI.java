ccddc2f5ec8403cf6e3585f339cf24d151d4b948
Fabio Anderegg <andef4@bfh.ch>
2014-01-13 23:31:21 +0100

    added WizardAI

--- src/src/main/java/ch/andefgassm/adventuregame/game/ai/WizardAI.java
+++ src/src/main/java/ch/andefgassm/adventuregame/game/ai/WizardAI.java
@@ -1,9 +1,16 @@
 package ch.andefgassm.adventuregame.game.ai;
 
+import java.util.Random;
+
 import ch.andefgassm.adventuregame.combat.AbstractAICombatant;
 import ch.andefgassm.adventuregame.combat.CombatSystem;
 
-public class WizardAI extends AbstractAICombatant{
+public class WizardAI extends AbstractAICombatant {
+
+    enum Phase {DAMAGE, HEAL};
+    private Phase phase = Phase.DAMAGE;
+    private Random random = new Random();
+    private int currentRound = 0;
 
     public WizardAI(CombatSystem system, String name, int maxLife) {
         super(system, name, maxLife);
@@ -14,6 +21,32 @@ public class WizardAI extends AbstractAICombatant{
 
     @Override
     public String getNextSkill() {
-        return getAvailableSkills().get(0);
+        String skill = null;
+        if (currentRound >= 3) {
+            if (phase == Phase.DAMAGE) {
+                phase = Phase.HEAL;
+            } else {
+                phase = Phase.DAMAGE;
+            }
+            currentRound = 0;
+        }
+
+        switch (phase) {
+        case DAMAGE:
+            if (random.nextFloat() > 0.5) {
+                skill = "wizard_tempest";
+            } else {
+                skill = "wizard_flash";
+            }
+            break;
+        case HEAL:
+            if (random.nextFloat() > 0.5) {
+                skill = "wizard_heal";
+            } else {
+                skill = "wizard_flash";
+            }
+        }
+        currentRound++;
+        return skill;
     }
 }
